b4d09be528f87c792a14cfb240d72fe43b1115e4
pglekshmi <pglekshmi88@gmail.com>
2023-12-08 17:18:30 +0530

    update

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express,{json} from "express";
 import apiroutes from "./api-routes.js";
+import logger from "morgan";
 const listen_port=8000;
 const app=express();
-app.use('/',apiroutes);
+app.use(logger("dev"));
 app.use(json());
+app.use('/',apiroutes);
 app.listen(listen_port,()=>{
     console.log(`Listening to port${listen_port}`)
 })
