961e5569aa624ffa315ee98896fd200002902030
pglekshmi <pglekshmi88@gmail.com>
2023-12-08 16:15:21 +0530

    update

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import apiroutes from "./api-routes.js";
 const listen_port=8000;
 const app=express();
+app.use('/',apiroutes);
 app.listen(listen_port,()=>{
     console.log(`Listening to port${listen_port}`)
 })
