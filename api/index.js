ab7192f0c0b90e0bc2a860915c0c6c165f0e2234
lekshmi <pglekshmi88@gmail.com>
2023-12-09 12:56:14 +0530

    updated ui

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express,{json} from "express";
+import cors from "cors";
 import apiroutes from "./api-routes.js";
 import logger from "morgan";
 const listen_port=8000;
 const app=express();
+app.use(cors({origin:'*'}))
 app.use(logger("dev"));
 app.use(json());
 app.use('/',apiroutes);
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,13 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[inputMsg,setinputMsg]=useState();
+
+  function msgHandler(event){
+    setinputMsg(event.target.value);
+  }
+
+  async function setValue(){
+     const response = await fetch('http://127.0.0.1:8000/create',{
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({inputMsg})
+     })
+    //  console.log(response.status);
+    //  if(response.status=201){
+    //   alert("Sucessfully sent to api");
+    //  }
+    //  else{
+    //   alert("Something is wrong");
+    //  }
+  } 
+
+
+
   return (
     <div className='flex flex-col justify-center items-center h-screen'>   
    <p className='font-bold text-2xl py-4'>Sample Dapp</p>
    <p className='font-bold text-xl py-4'>Set Message</p>
-   <input type='text'className='border border-gray-400 py-4 px-6 rounded' placeholder='Enter your Message'></input><br/>
-   <button className='bg-blue-400 hover:bg-blue-700 rounded text-white py-2 px-6'>Set</button>
+   <input type='text' onChange={msgHandler} className='border border-gray-400 py-4 px-6 rounded' placeholder='Enter your Message'></input><br/>
+   <button onClick={setValue} className='bg-blue-400 hover:bg-blue-700 rounded text-white py-2 px-6'>Set</button>
    <p className='font-bold text-xl py-4'>Get Message</p>
    <button className='bg-blue-400 hover:bg-blue-700 rounded text-white py-2 px-6'>Get</button>
    <p className='font-bold text-2xl py-4' ></p>
