f922ba018ced11d17487b7563a740abf91c8ba05
tr1sm0s1n <mobinm.mohanan@gmail.com>
2024-04-16 20:38:33 +0530

    ⚡️ | updated ui

--- ui-new/src/App.jsx
+++ ui-new/src/App.jsx
@@ -1,34 +1,62 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState()
+  const [output, setOutput] = useState()
+
+  function handleChange(event) {
+    setInput(event.target.value)
+  }
+
+  async function setValue() {
+    const response = await fetch('http://127.0.0.1:8080/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ input }),
+    })
+    if (response.ok) {
+      alert('Set Successfully')
+    } else {
+      alert('Failed to Set')
+    }
+  }
+
+  async function getValue() {
+    const res = await fetch('http://127.0.0.1:8080/read')
+    if (res.status == 200) {
+      const result = await res.json()
+      setOutput(result)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <p className="font-bold text-2xl py-4">Sample Dapp</p>
+      <p className="font-bold text-xl py-4">Set Message</p>
+      <input
+        type="text"
+        onChange={handleChange}
+        className="border border-gray-400 py-4 px-6 rounded"
+        placeholder="Enter your Message"
+      ></input>
+      <br />
+      <button
+        onClick={setValue}
+        className="bg-blue-400 hover:bg-blue-700 rounded text-white py-2 px-6"
+      >
+        Set
+      </button>
+      <p className="font-bold text-xl py-4">Get Message</p>
+      <button
+        onClick={getValue}
+        className="bg-blue-400 hover:bg-blue-700 rounded text-white py-2 px-6"
+      >
+        Get
+      </button>
+      <p className="font-bold text-2xl py-4">{output}</p>
+    </div>
   )
 }
 
